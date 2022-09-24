47b78535668acea2dfff69252df5f288f816c30b
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2022-09-24 15:07:44 +0000

    2022/09/24

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
   end
 
   def destroy
-     book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
     redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end

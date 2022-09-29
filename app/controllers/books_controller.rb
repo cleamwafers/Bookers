640bdaccf0e0d8b961ce5921ed50cbbce1b0ba9a
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2022-09-29 11:03:05 +0000

    2022/09/29 02

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,12 +10,14 @@ protect_from_forgery
    if @book.save
     redirect_to book_path(@book.id), notice: "Book was successfully created."
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
